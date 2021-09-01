37bfa5158e19d42383cdc0ecd838980a868b817b
yjk5309 <yunjookim97@daum.net>
2021-09-01 10:08:55 +0900

    객체지향 프로그래밍 인터페이스

---
+++ Lion.java
@@ -0,0 +1,5 @@
+public class Lion extends Animal implements Predator {
+    public String getFood() {
+        return "banana";
+    }
+}
---
+++ ZooKeeper.java
@@ -0,0 +1,5 @@
+public class ZooKeeper {    
+    public void feed(Predator predator) {
+        System.out.println("feed "+predator.getFood());
+    }
+}
