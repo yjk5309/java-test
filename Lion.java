c02cd8d4d9243e35045a5e458225eabbf593211d
yjk5309 <yunjookim97@daum.net>
2021-09-01 10:56:26 +0900

    객체지향 프로그래밍 다형성

--- Lion.java
+++ Lion.java
@@ -1,5 +1,8 @@
-public class Lion extends Animal implements Predator {
+public class Lion extends Animal implements Predator, Barkable {
     public String getFood() {
         return "banana";
+    }
+    public void bark() {
+        System.out.println("으르렁");
     }
 }
--- Tiger.java
+++ Tiger.java
@@ -1,5 +1,9 @@
-public class Tiger extends Animal implements Predator {
+public class Tiger extends Animal implements Predator, Barkable {
     public String getFood() {
         return "apple";
+    }
+
+    public void bark() {
+        System.out.println("어흥");
     }
 }
