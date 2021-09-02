675acd4a5b0b6d741d61aa72d6fdbd1307541f3f
yjk5309 <yunjookim97@daum.net>
2021-09-02 11:13:43 +0900

    싱글톤 패턴

---
+++ SingletonTest.java
@@ -0,0 +1,20 @@
+class Singleton {
+    private static Singleton one;
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if(one==null) {
+            one = new Singleton();
+        }
+        return one;
+    }
+}
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);
+    }
+}
